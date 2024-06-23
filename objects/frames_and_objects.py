4f94535360e074f91fab1f24396adf2030a2a705
sv <shankarv.alt@gmail.com>
2024-06-23 13:12:17 +0100

    Debugging, Simultinaeity demo@

--- objects/frames_and_objects.py
+++ objects/frames_and_objects.py
@@ -1,19 +1,28 @@
 import numpy as np
 
-minkowski_metric = np.diag([1,-1,-1,-1])    
+minkowski_metric = np.diag([1,-1,-1,-1])
+
+def gamma(v):
+    return np.sqrt(1/(1-v**2))
 
 class Frame:
     
     def __init__(self, lab_lorentz):
         # lab_lorentz is the nxn matrix gets us from the lab frame to the frame we're working in
-        if ((lab_lorentz.T).dot(minkowski_metric).dot(lab_lorentz) != minkowski_metric).any():
-            raise TypeError("lab_lorentz is not a Lorentz transform (failed to preserve minkowski_metric)")
+        #if ((lab_lorentz.T).dot(minkowski_metric).dot(lab_lorentz) != minkowski_metric).any():
+        #   raise TypeError("lab_lorentz is not a Lorentz transform (failed to preserve minkowski_metric)")
         self.lab_lorentz = lab_lorentz
 
     def equals(self, frame):
         return (self.lab_lorentz == frame.lab_lorentz).all()
 
-
+    def boost_x(self, v):
+        g = gamma(v)
+        boosted_lorentz = np.array([[g, -g*v, 0, 0],
+                           [-g*v, g, 0, 0],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]]).dot(self.lab_lorentz)
+        return Frame(boosted_lorentz)
 
 class FourVector:
 
@@ -24,23 +33,30 @@ class FourVector:
         self.labvec = None
 
     def get_labvec(self):
-        if labvec is None:
-            inverse_lorentz = np.linalg.inv(frame)
-            self.labvec = inverse_lorentz.dot(fourvec)
+        if self.labvec is None:
+            inverse_lorentz = np.linalg.inv(self.frame.lab_lorentz)
+            self.labvec = inverse_lorentz.dot(self.fourvec)
         return self.labvec
 
     def change_frame(self, newframe):
-        self.fourvec = newframe.dot(self.get_labvec)
+        self.fourvec = (newframe.lab_lorentz).dot(self.get_labvec())
         self.frame = newframe
 
-    def inner_producter_product(self, X):
+    def inner_product(self, X):
         if not self.frame.equals(X.frame):
             raise ValueError("2 FourVector s are not in the same frame")
         return (self.fourvec.T).dot(minkowski_metric).dot(X.fourvec)
+
+    def separation(self, X):
+        if self.inner_product(X) < 0:
+            return "Timelike"
+        elif self.inner_product(X) == 0:
+            return "Lightlike"
+        else:
+            return "Spacelike"
 
     def add(self, X):
         if not self.frame.equals(X.frame):
             raise ValueError("2 FourVector s are not in the same frame")
         
 
-        
---
+++ objects/simultinaeity_demo.py
@@ -0,0 +1,24 @@
+from frames_and_objects import *
+
+# We will create 2 events event1 and event2
+# These will be simultanous in the rest frame 
+# However as we will see, if we create a new frame
+# which is boosted from the rest frame at 0.5 c in the x direction
+# then they are no longer simultanous in this frame
+# However, their inner product is consistent, as is their separation
+
+lab_frame = Frame(np.identity(4))
+event1 = FourVector(np.array([1,0,0,0]), lab_frame)
+event2 = FourVector(np.array([1,1,0,0]), lab_frame)
+print("Event 1 and 2 in lab frame:")
+print("event1 happens at   " + str(event1.fourvec))
+print("event2 happens at   " + str(event2.fourvec))
+print("inner product is   " + str(event1.inner_product(event2)))
+boosted_frame = lab_frame.boost_x(0.5)
+event1.change_frame(boosted_frame)
+event2.change_frame(boosted_frame)
+print("Event 1 and 2 in boosted frame:")
+print("event1 happens at   " + str(event1.fourvec))
+print("event2 happens at   " + str(event2.fourvec))
+print("inner product is   " + str(event1.inner_product(event2)))
+
